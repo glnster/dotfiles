5643797f9d090ca72c64552f75c88fc3c90e4d20
Glenn Cueto <glnster@yahoo.com>
2014-12-26 13:33:29 -0800

    Update setup.sh to cp .gitconfig to ~/
    Use ~/.gitconfig (not symlinked) because older Git installations don't do include paths. :(

--- setup.sh
+++ setup.sh
@@ -9,19 +9,43 @@ dots=('.aliases'
       '.bashrc'
       '.exports'
       '.functions'
+      #'.gitconfig'  setup as local config file with credentials/settings
+      '.gitignore_global'
       '.vim'
       '.vimrc'
       '.vimrc.bundles'
       '.zshrc'
-      '.gitconfig'
-      '.gitignore_global'
       );
 
-# TODO check if file exists in dotfiles before rm. Else alert and skip.
+copies=('.extra'
+        '.gitconfig'
+        );
 
 for dot in "${dots[@]}"
 do
-  rm $dot
-  ln -s dotfiles/$dot $dot
+  ln -sf dotfiles/$dot $dot
 done
 
+echo "» In $PWD"
+
+for copy in "${copies[@]}"
+do
+  if [ -f $copy ]; then
+    echo "» Saving $copy as $copy.bak"
+    cp $copy $copy.bak
+  fi
+done
+
+# Copy files to ~/. Prompt for overwriting.
+echo
+cp -Ri dotfiles/copy/. .
+
+echo
+while true; do
+    read -p "Edit .gitconfig credentials? y/n : " yn
+    case $yn in
+        [Yy]* ) "${EDITOR:-vi}" .gitconfig; break;;
+        [Nn]* ) break;;
+        * ) echo "Please answer y or n.";;
+    esac
+done
